d798767f4716e5dcf4ccb5f776e87c63688d9517
Exequiel Ceasar Navarrete <dev@exequielnavarrete.me>
2022-06-14 16:46:56 +0800

    initial commit

---
+++ apps/rxa-cdk-scheduler-issue/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rxa-cdk-scheduler-issue-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'rxa-cdk-scheduler-issue';
+}
